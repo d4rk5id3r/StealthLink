dada4e411d554c4e7c42cfe927da6efcf2b386cd
d4rk5id3 <121777365+d4rk5id3r@users.noreply.github.com>
2023-04-30 11:57:16 +0100

    Update Stealthlink.py

--- Stealthlink.py
+++ Stealthlink.py
@@ -1,10 +1,12 @@
 
 #!/usr/bin/env python3
-from termcolor import colored
-import subprocess
-import time
+# Importing necessary modules
+from termcolor import colored # for colored text output
+import subprocess  # for running shell commands
+import time  # for time-related functions
 
 #Colors
+# Defining colors for text output
 COLOR_YELLOW = '\033[93m'
 COLOR_BLUE = '\033[94m'
 COLOR_MAGENTA = '\033[95m'
@@ -14,6 +16,7 @@ COLOR_GREEN = '\033[92m'
 COLOR_RED = '\033[91m'
 COLOR_RESET = '\033[0m'
 
+# Defining colors for ASCII text output
 Banner = '\033[95m' + ''' .
 
 
@@ -33,27 +36,32 @@ Banner = '\033[95m' + ''' .
 
  ''' + '\033[0m'
 
+# Printing the banner
 print(Banner)
 
-
+# Printing some introductory text
 print(COLOR_BLUE + "[" + COLOR_RESET + COLOR_YELLOW + "Stealth-Link" + COLOR_RESET  + COLOR_BLUE + "] " "Developed by D4RK5ID3 " + COLOR_RESET + COLOR_YELLOW + "Welcome to Stealth-Link script v1.0" + COLOR_RESET)
 
-
+# Function to validate the URL input
 def url_validator(url):
     if not url.startswith('http') and not url.startswith('https'):
         print(COLOR_RED + "[!] Invalid URL. Please use http or https." + COLOR_RESET)
         exit(1)
 
+
+# Getting input URL from the user and validating it
 link = input(COLOR_BLUE + "Input your Phishing URL (http or https): " + COLOR_RESET)
 
 url_validator(link)
 
+# Rebuilding the phishing URL using a web API
 print(COLOR_YELLOW + "Rebuilding Phishing URL" + COLOR_RESET)
 
-time.sleep (1)
+time.sleep (1) # Sleep for 1 second
 
 print("")
 
+# Compressing the URL using the is.gd API
 compress = f"curl -s 'https://is.gd/create.php?format=simple&url={link}'"
 
 comp = subprocess.check_output(compress, shell=True).decode('utf-8').strip()
@@ -66,10 +74,12 @@ mask = input(f"{COLOR_BLUE}Enter a Domain To Conceal The Phishing URL (http or h
 
 url_validator(mask)
 
+# add some social engineering words to enabe link tolook more convincing
 words = input("Type social engineering words:(like free-money, best-pubg-tricks)\n {COLOR_RED} Don't use space just use '-' between social engineering words\n=> " + COLOR_RESET)
 
 print(COLOR_GREEN + "\n Generating StealthLink ...\n " + COLOR_RESET)
 
+# print final Phishing link
 final = f"{mask}-{words}@{compressed}"
 
 print(f"{COLOR_GREEN}Here is the Modified StealthLink URL: \033[32m {final} \033[0m\n ")
